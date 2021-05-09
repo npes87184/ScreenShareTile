ae633ee17270e1f7394440599f601c00bb7584f8
Yu-Chen Lin <npes87184@gmail.com>
2021-05-09 13:50:13 +0800

    Change define class to object
    Signed-off-by: Yu-Chen Lin <npes87184@gmail.com>

--- app/src/main/java/com/npes87184/screenshottile/utils/Define.kt
+++ app/src/main/java/com/npes87184/screenshottile/utils/Define.kt
@@ -1,10 +1,8 @@
 package com.npes87184.screenshottile.utils
 
-class Define {
-    companion object {
-        const val DELAY_TIME = "delayTime"
-        const val DELAY_TIME_DEFAULT = "250"
-        const val MY_WEB_URL = "https://npes87184.github.io"
-        const val APP_URL = "market://details?id=com.npes87184.screenshottile"
-    }
+object Define {
+    const val DELAY_TIME = "delayTime"
+    const val DELAY_TIME_DEFAULT = "250"
+    const val MY_WEB_URL = "https://npes87184.github.io"
+    const val APP_URL = "market://details?id=com.npes87184.screenshottile"
 }
